83e61a23258c7cb321d33458a3c9783660ffb278
Nicholas Babo <nickbabo7@gmail.com>
2017-05-25 14:54:52 -0300

    Implementação teste do mecanismo de verificar pra onde o usuario esta olhando

--- Guback/ViewController.swift
+++ Guback/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var row: UILabel!
+    
+    @IBOutlet weak var yaw: UILabel!
 
+    @IBOutlet weak var pitch: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let manager = CMMotionManager()
+        
+        if manager.isDeviceMotionAvailable{
+            manager.deviceMotionUpdateInterval = 0.1
+            manager.startDeviceMotionUpdates(using: CMAttitudeReferenceFrame.xTrueNorthZVertical, to: OperationQueue.main, withHandler: {
+                (_, error) in
+                if error != nil{
+                    print ("Deu onda")
+                }else{
+                    if let data = manager.deviceMotion?.attitude{
+                        let yaw = Float(data.yaw * 180 / Double.pi)
+                        
+                        if yaw <= -80 && yaw >= -170{
+                            self.row.text = "FCI"
+                        }else{
+                            self.row.text = "hmmmm"
+                        }
+                    }
+                }
+            })
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
